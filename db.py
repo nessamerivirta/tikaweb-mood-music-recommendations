069ed98fa3b58dc951efb335d33d841cba617902
Nessa Merivirta <nessamerivirta@gmail.com>
2026-03-18 15:06:42 +0200

    Database creation and search works

--- db.py
+++ db.py
@@ -10,3 +10,10 @@ def query(sql, params=()):
     results = cursor.fetchall()
     conn.close()
     return results
+
+def execute(sql, params=()):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(sql, params)
+    conn.commit()
+    conn.close()
