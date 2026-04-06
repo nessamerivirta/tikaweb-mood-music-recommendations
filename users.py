df763df1dea6092ce6a93b1f7e0d516ec0375447
Nessa Merivirta <nessamerivirta@gmail.com>
2026-04-06 16:54:31 +0300

    Functions for collecting user data created

---
+++ users.py
@@ -0,0 +1,13 @@
+import db
+
+def get_user(user_id):
+    sql = "SELECT id, username FROM users WHERE id = ?"
+    result = db.query(sql, [user_id])
+    return result[0] if result else None
+
+def get_posts(user_id):
+    sql = """SELECT p.id, p.artist, p.song, p.comment, p.image_path, p.sent_at, p.user_id, p.category
+             FROM posts AS p
+             WHERE p.user_id = ?
+             ORDER BY p.sent_at DESC"""
+    return db.query(sql, [user_id])
