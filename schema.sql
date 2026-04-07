785059fa7c0e4242f427ba66d311e64e065078bf
Nessa Merivirta <nessamerivirta@gmail.com>
2026-04-07 11:39:52 +0300

    Database schema updated with the likes table

--- schema.sql
+++ schema.sql
@@ -11,6 +11,16 @@ CREATE TABLE posts (
     comment TEXT,
     image_path TEXT,
     sent_at TEXT,
-    user_id INTEGER REFERENCES users
-
+    user_id INTEGER REFERENCES users,
+    category TEXT
 );
+
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, post_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (post_id) REFERENCES posts (id)
+)
