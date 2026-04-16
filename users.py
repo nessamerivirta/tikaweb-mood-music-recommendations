892f046e9c2b06e432691871847504192ec289fa
Nessa Merivirta <nessamerivirta@gmail.com>
2026-04-16 19:33:17 +0300

    Genre added to the SQL query for user's posts

--- users.py
+++ users.py
@@ -7,7 +7,7 @@ def get_user(user_id):
     return result[0] if result else None
 
 def get_posts(user_id):
-    sql = """SELECT p.id, p.artist, p.song, p.comment, p.image_path, p.sent_at, p.user_id, p.category
+    sql = """SELECT p.id, p.artist, p.song, p.comment, p.image_path, p.sent_at, p.user_id, p.genre, p.mood
              FROM posts AS p
              WHERE p.user_id = ?
              ORDER BY p.sent_at DESC"""
